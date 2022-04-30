cbf7cb8e6beb2a27cc977c89a3fab8a4d045232f
Force-Developing <79415118+Force-Developing@users.noreply.github.com>
2022-04-30 10:42:56 +0200

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,14 +1,27 @@
+ESX              = nil
+local PlayerData = {}
+
 Citizen.CreateThread(function()
     while ESX == nil do
-        Citizen.Wait(1);
-
-        ESX = exports["btrp_base"]:getSharedObject()  
-    end 
-
-    if ESX.IsPlayerLoaded() then
-        ESX.PlayerData = ESX.GetPlayerData()
+      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+      Citizen.Wait(0)
     end
+    Citizen.Wait(500)
+  
+    PlayerData = ESX.GetPlayerData()
+  end)
 
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  PlayerData.job = job
+end)
+
+Citizen.CreateThread(function()
     while true do
 		local sleepThread = 850
 		local player = PlayerPedId()
@@ -27,7 +40,7 @@ Citizen.CreateThread(function()
 			end
 		end
 
-        if ESX.PlayerData.job ~= nil and ESX.PlayerData.job.name == 'police' then
+        if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
             if dst >= 1.5 and dst <= 6.0 then
                 sleepThread = 5
                 Funcs.Text3D(Config.MarkerPos, "Garage")
@@ -55,24 +68,23 @@ Citizen.CreateThread(function()
                     end
                 end
             end
-        end
 
-        if dst2 >= 1.5 and dst2 <= 7.0 then
-            sleepThread = 5
-            Funcs.Text3D(Config.SpawnVehiclePos, "Parkera")
-            DrawMarker(6, Config.SpawnVehiclePos.x, Config.SpawnVehiclePos.y, Config.SpawnVehiclePos.z - 0.985, 0.0, 0.0, 0.0, 270.0, 0.0, 0.0, 5.0, 5.0, 5.0, 255, 0, 0, 100, false, true, 2, false, false, false, false)
-        end
+            if dst2 >= 1.5 and dst2 <= 7.0 then
+                sleepThread = 5
+                Funcs.Text3D(Config.SpawnVehiclePos, "Parkera")
+                DrawMarker(6, Config.SpawnVehiclePos.x, Config.SpawnVehiclePos.y, Config.SpawnVehiclePos.z - 0.985, 0.0, 0.0, 0.0, 270.0, 0.0, 0.0, 5.0, 5.0, 5.0, 255, 0, 0, 100, false, true, 2, false, false, false, false)
+            end
 
-        if dst2 <= 1.5 then
-            sleepThread = 5
-            Funcs.Text3D(Config.SpawnVehiclePos, "[~p~E~s~] Parkera")
-            DrawMarker(6, Config.SpawnVehiclePos.x, Config.SpawnVehiclePos.y, Config.SpawnVehiclePos.z - 0.985, 0.0, 0.0, 0.0, 270.0, 0.0, 0.0, 5.0, 5.0, 5.0, 0, 255, 0, 100, false, true, 2, false, false, false, false)
-            
-            if IsControlJustPressed(1, 38) then
-                ESX.Game.DeleteVehicle(GetVehiclePedIsIn(PlayerPedId(), false))
+            if dst2 <= 1.5 then
+                sleepThread = 5
+                Funcs.Text3D(Config.SpawnVehiclePos, "[~p~E~s~] Parkera")
+                DrawMarker(6, Config.SpawnVehiclePos.x, Config.SpawnVehiclePos.y, Config.SpawnVehiclePos.z - 0.985, 0.0, 0.0, 0.0, 270.0, 0.0, 0.0, 5.0, 5.0, 5.0, 0, 255, 0, 100, false, true, 2, false, false, false, false)
+                
+                if IsControlJustPressed(1, 38) then
+                    ESX.Game.DeleteVehicle(GetVehiclePedIsIn(PlayerPedId(), false))
+                end
             end
         end
         Wait(sleepThread)
     end
 end)
-
